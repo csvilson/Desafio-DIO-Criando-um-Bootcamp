247a8a63708ddfd17b3b83b549ea9ee0c57f059d
csvilson <csvilson@gmail.com>
2023-03-25 10:46:21 -0300

    Criação das classes Curso e Mentoria

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
+
+        Curso curso1 = new Curso();
+        System.out.println(curso1);
+
+        Mentoria mentoria1 = new Mentoria();
+        System.out.println(mentoria1);
+
+
+
+
+
+    }//main
+}//MAIN
